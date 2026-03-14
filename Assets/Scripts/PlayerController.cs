85e112ab19633ca771083a9e812f4e99525f5958
Alexander Bessalov <bessalovs@gmail.com>
2026-03-14 19:20:29 +0300

    Упорядочил иерархию и укомплектовал скрипт для читабельности

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,16 +14,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ConfigControlPlayer();
+        ConstrainPlayerPosition();
+    }
+
+    //Настройка управления игрока
+    void ConfigControlPlayer()
+    {
         float horizontalSpeed = Input.GetAxis("Horizontal");
         float verticalSpeed = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * verticalSpeed * speed);
         playerRb.AddForce(Vector3.right * horizontalSpeed * speed);
+    }
 
+    //Настройка невидимых препятствий
+    void ConstrainPlayerPosition()
+    {
         if (transform.position.z > zBounds)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zBounds);
-        } else if (transform.position.z < -zBounds)
+        }
+        else if (transform.position.z < -zBounds)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -zBounds);
         }
