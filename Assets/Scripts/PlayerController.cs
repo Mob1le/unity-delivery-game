257ed17ce1bf43fefbcfa7c38f440577e9928e58
Alexander Bessalov <bessalovs@gmail.com>
2026-03-14 11:43:38 +0300

    Added PlayerController logic and walls on the horizonal sides
    Made ability for moving Player
    Made for walls the purple material
    Setting on rotations of player constraint for X and Z axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalSpeed = Input.GetAxis("Horizontal");
+        float verticalSpeed = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * verticalSpeed * speed);
+        playerRb.AddForce(Vector3.right * horizontalSpeed * speed);
     }
 }
